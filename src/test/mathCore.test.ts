264db9015535e30f308db483a7ca2779293b37ab
Arslan Dev <arslan.dev@gmail.com>
2023-09-27 10:33:35 +0400

    Test all available math functions

--- src/test/mathCore.test.ts
+++ src/test/mathCore.test.ts
@@ -6,4 +6,24 @@ describe('Test Math functions', () => {
     const actualSum = calculate(2, 3, EOperator.Addition)
     expect(actualSum).eq(5)
   })
+
+  it('should subtract two numbers', () => {
+    const actualDifference = calculate(2, 3, EOperator.Subtraction)
+    expect(actualDifference).eq(-1)
+  })
+
+  it('should multiplicate two numbers', () => {
+    const actualProduct = calculate(2, 3, EOperator.Multiplication)
+    expect(actualProduct).eq(6)
+  })
+
+  it('should divide two numbers', () => {
+    const actualQuotient = calculate(3, 2, EOperator.Division)
+    expect(actualQuotient).eq(1.5)
+  })
+
+  it('should not divide by zero', () => {
+    const actualQuotient = calculate(3, 0, EOperator.Division)
+    expect(actualQuotient).eq("Error")
+  })
 })
